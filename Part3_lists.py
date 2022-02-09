6e4d6acb3283e9f24e4f0fb9334edaf055744f50
Ying Chiu Goh <Ying.Goh@learn.cpp.ac.nz>
2022-02-09 13:13:03 +1300

    python list example

---
+++ Part3_lists.py
@@ -0,0 +1,45 @@
+mylist = ['a','b','c','d','e']
+print(mylist[-1])
+print(mylist[:1])
+print(mylist[:3])
+
+print('before reassignment: ')
+print(mylist)
+mylist[0] = 'New Item'
+print('after reassignment: ')
+print(mylist)
+
+mylist.append('New things')
+print(mylist)
+
+listtwo = ['f','g','h','i']
+# mylist.append(listtwo)
+# print('append array: ')
+# print(mylist)
+
+mylist.extend(listtwo)
+print('extend array: ')
+print(mylist)
+
+item = mylist.pop()
+print(mylist)
+print(item)
+
+mylist.reverse()
+print(mylist)
+
+numberlist = [5, 9,23,1,78]
+numberlist.sort()
+print(numberlist)
+
+newlist = [1,2,['x','y','s']]
+print(newlist[2])
+print(newlist[2][2])
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(matrix[0][0])
+
+#get first index of each col
+first_col = [row[0] for row in matrix]
+print(first_col)
+
