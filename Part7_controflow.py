98c89b232a03e505750a890b9b17dbe3dd2bbb52
YingCG <yingchiugoh@gmail.com>
2022-02-09 23:45:01 +1300

    loop, control flow

--- Part7_controflow.py
+++ Part7_controflow.py
@@ -26,3 +26,57 @@ elif 3 ==3 :
     print('elif ran')
 else: 
     print('last')
+
+# For Loops
+seq = [1, 2, 3, 4, 5, 6]
+
+for jelly in seq:
+    print(jelly)
+
+cafe ={"coffee": 1, "bread": 2, "muffin":3}
+for item in cafe:
+    print(item)
+
+for k in cafe:
+    print(k)
+    print(cafe[k])
+
+mypairs = [(1,2),(3,4),(5,6)]
+
+for item in mypairs:
+    print(item)
+
+for (item1, item2) in mypairs:
+    print(item1)
+    print(item2)
+
+for tup1, tup2 in mypairs:
+    print(tup2)
+    print(tup1)   
+
+# While Loops
+    print('')   
+    print("While loops")   
+
+i = 1
+while i < 5:
+    print("i is: {}".format(i))
+    i = i +1
+
+# range
+numbers = list(range(0,5))
+print(numbers)
+
+for things in range(10):
+    print(things)
+
+# List Comprehension
+x = [1,2,3,4]
+out = []
+for num in x:
+    out.append(num**2) #power of 2
+
+print(out)
+
+square = [num**2 for num in x]
+print(square)
