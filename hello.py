b15e963828525121063e19c29683d02022e626e5
YingCG <yingchiugoh@gmail.com>
2023-03-12 22:30:15 +1300

    syntax exercise

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+print("Hello " * 5)
+username = input("Enter username:")
+def greeting():
+    username = ''
+    if not username:
+        print("Hello World")
+
+    else:
+        print("Hello " + username)
+
+
+greeting()
