c5ec9fcb093a57a02a9555dd25a02fce2ab0e6bc
Ying Chiu Goh <YingG@whiteclife.ac.nz>
2023-05-30 16:52:49 +1200

    nesting and dictionary refresher

---
+++ .history/01 SyntaxPractice/Part17_Dictionaries/Nesting/gradingProgram_20230530131522.py
@@ -0,0 +1,12 @@
+student_scores = {
+    "Harry" : 81,
+    "Ron" : 78,
+    "Hermione" : 89,
+    "Draco" : 74,
+    "Neville" : 62
+}
+
+scoring_grades = {}
+
+for student in student_scores:
+       print(student)
